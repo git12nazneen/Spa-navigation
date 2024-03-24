f4293405b414fd8f01ca6960279e51cf7551e791
git12nazneen <lipinazneen65@gmail.com>
2024-03-25 03:40:45 +0600

    card

---
+++ src/Components/Blogcard.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import placeholderImage from '../assets/404.jpg'
+
+const Blogcard = ({blog}) => {
+    const {cover_image,title,description,id,published_at} = blog
+    return (
+        <div>
+           <Link to={`/blog/${id}`} className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 transition border-2 border-primary hover:border-secondary border-opacity-30 hover:scale-105">
+				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image || placeholderImage} />
+				<div className="p-6 space-y-2">
+					<h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+					<span className="text-xs dark:text-gray-600">{new Date(published_at).toLocaleDateString()}</span>
+					<p>{description}</p>
+				</div>
+			</Link>
+        </div>
+    );
+};
+
+export default Blogcard;
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="hero -mt-16">
             <div className="hero-content text-center">
                 <div className="max-w-lg">
-                <h1 className="text-5xl font-bold">Welcome to <span>ByteBlaze</span></h1>
+                <h1 className="text-5xl font-bold">Welcome to {' '} <span className=" bg-gradient-to-r bg-300% from-primary via-blue-500 to-secondary text-transparent bg-clip-text animate-gradient">ByteBlaze</span></h1>
                 <p className="py-6">ByteBlaze is the bridge between the complex world of technology and the curious minds eager to understand it</p>
                 <div className="flex gap-2 justify-center">
 

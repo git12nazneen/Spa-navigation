a4392c61fc551e3316d78e38581ecb262c439861
git12nazneen <lipinazneen65@gmail.com>
2024-03-25 01:37:09 +0600

    first commit

---
+++ src/Pages/Bookmarks.jsx
@@ -0,0 +1,11 @@
+
+
+const Bookmarks = () => {
+    return (
+        <div>
+            <h1>books mark</h1>
+        </div>
+    );
+};
+
+export default Bookmarks;<h1>books mark</h1>
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from "../Components/Hero";
+import wave from '../assets/wave.svg'
+
+
+const Home = () => {
+    return (
+        <div className="relative flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
+            <Hero></Hero>
+            <img className="absolute bottom-0 w-full" src={wave} alt="" />
+        </div>
+    );
+};
+
+export default Home;
